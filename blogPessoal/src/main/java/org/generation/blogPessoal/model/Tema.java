9797ab13353f297f6d1949b28bcbb9e076ce79db
AndreHOSilva <andrehenrique2020@hotmail.com>
2021-08-05 15:48:59 -0300

    Atualizacoes do blog

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Tema.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Tema.java
@@ -2,13 +2,17 @@ package org.generation.blogPessoal.model;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sun.istack.NotNull;
 
 @Entity
 @Table(name = "tema")
@@ -18,12 +22,36 @@ public class Tema {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id ;
 	
-	@NotBlank
-	@Size(min = 5 , max = 100)
+	@NotNull
 	private String descricao ;
 	
-	@NotBlank
-	
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("tema")
 	private List<Postagem> postagem ;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+	
 	
 }
